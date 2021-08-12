2bbfc354a5edb54ac81ef690c0522fdbf5ed0691
RhaegarPrin <linhlikconj3@gmail.com>
2021-08-12 10:07:59 +0700

    add hr role button , delete button pm

--- models/hr_rq.py
+++ models/hr_rq.py
@@ -6,3 +6,4 @@ class hr_req(models.Model):
 
     pm_reqs = fields.One2many('pm.req', 'hr_id', string="List_req")
     rela_user = fields.Many2one('res.users', string='USER Related', default=lambda self: self.env.user)
+
