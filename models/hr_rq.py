96d76c1376975fdaa9419598f0c37c1982205b2c
RhaegarPrin <linhlikconj3@gmail.com>
2021-08-12 10:46:40 +0700

    check hr menu

--- models/hr_rq.py
+++ models/hr_rq.py
@@ -7,3 +7,4 @@ class hr_req(models.Model):
     pm_reqs = fields.One2many('pm.req', 'hr_id', string="List_PM_req")
     rela_user = fields.Many2one('res.users', string='USER Related', default=lambda self: self.env.user)
     emp_reqs = fields.One2many('employee.req', 'pm_id', string='EMP reqs')
+
